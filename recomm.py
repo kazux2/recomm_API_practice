e907a3fbf10e30c9472d14d27041f387ec4130be
KAZUKI OZONE <kzkz.ozn@gmail.com>
2017-11-10 17:33:49 +0900

    copied npy file save test in model.py

--- recomm.py
+++ recomm.py
@@ -1,23 +1,6 @@
 import numpy as np
 
 scores = np.loadtxt('sushi3b.5000.10.score', delimiter=' ')
-"""numpy save test"""
-print('This is scores')
-print(scores)
-np.save('scores1.npy', scores)
-
-scores1_load = np.load('scores1.npy')
-print('This is scores_load saved as scores.npy')
-print(scores1_load)
-
-scores1_load[1,0:99] = 10
-print('scores1_load renewed')
-print(scores1_load)
-np.save('scores2.npy', scores1_load)
-
-scores2_load = np.load('scores2.npy')
-print('This is scores2_load saved as scores2.npy')
-print(scores2_load)
 
 def get_correlation_coefficents(scores, target_user_index):
     similarities = []
